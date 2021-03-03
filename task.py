0348b1571668503fb76635e631bf1570d64587b5
JOY SINHA <51963863+sinhajoy@users.noreply.github.com>
2021-03-03 18:59:01 +0530

    Update task.py

--- task.py
+++ task.py
@@ -1,7 +1,31 @@
-import json
+# getting input from the user and assigning it to user
 
-with open("data.json", "w") as write_file:
-    json.dump(data, write_file)
-    
-    
+cm = float(input("Enter height in cm: "))
+height=cm / 100.0; 
+weight = float(input("Enter weight in kg: "))
+
+
+bmi = weight/height 
+
+
+
+
+print("Your BMI is: {0} and you are: ".format(bmi), end='')
+
+#conditions
+if ( bmi < 18.4):
+   print("Malnutrition risk")
+
+elif ( bmi >= 18.5 and bmi < 24.9):
+   print("Low risk")
+
+elif ( bmi >= 25 and bmi < 29.9):
+   print("Enhanced risk ")
+
+elif ( bmi >= 35 and bmi < 39.9):
+   print("Medium risk")
+
+elif ( bmi >=40):
+   print("High risk")
+
     
